5997dc85c460aa356d9e4110ebe6fc90415f6fbf
moroen <moroen@gmail.com>
2020-06-13 21:38:10 +0200

    Added ninja install target

--- meson_post_install.py
+++ meson_post_install.py
@@ -8,3 +8,4 @@ schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0'
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
+    subprocess.call(['update-desktop-database'])
