53116aac680126eb25085fe14db675e6898d1738
moroen <moroen@gmail.com>
2020-07-09 12:19:42 +0200

    Meson fix for mingw64

--- meson_post_install.py
+++ meson_post_install.py
@@ -2,10 +2,19 @@
 
 import os
 import subprocess
+import sys
 
-schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+# argv[1]: Meson host_system
+# argv[2]: Schema dir
+
+# schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+schemadir = sys.argv[2]
+
+print(sys.argv[1], schemadir)
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
-    subprocess.call(['update-desktop-database'])
+    
+    if sys.argv[1] == "linux":
+        subprocess.call(['update-desktop-database'])
