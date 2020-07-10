25bde62808bad615961dbe66bc1ee2cf9fcfd3a3
moroen <moroen@gmail.com>
2020-07-10 02:08:42 +0200

    Fixed meson for linux

--- meson_post_install.py
+++ meson_post_install.py
@@ -8,13 +8,11 @@ import sys
 # argv[2]: Schema dir
 
 # schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
-schemadir = sys.argv[2]
-
-print(sys.argv[1], schemadir)
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
-    subprocess.call(['glib-compile-schemas', schemadir])
+    subprocess.call(['glib-compile-schemas', sys.argv[2]])
     
     if sys.argv[1] == "linux":
+        print('Updating desktop database...')
         subprocess.call(['update-desktop-database'])
